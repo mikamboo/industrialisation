7dca833adb45be3317ee44d2ee66c5887d9c0e96
SaraMeb <43600049+SaraMeb@users.noreply.github.com>
2020-06-15 16:19:14 +0200

    Création d'un composant <Logo>
    Ajout du logo "IT-Akademy"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { LogoComponent } from "./logo/logo.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LogoComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
